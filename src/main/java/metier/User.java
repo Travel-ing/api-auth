5c4a764642dd06f67a610ad95a9c76383b3b2f96
marth154 <martin.danvers@gmail.com>
2022-12-08 18:54:35 +0100

    Authentification

---
+++ src/main/java/metier/User.java
@@ -0,0 +1,73 @@
+package metier;
+
+import org.bson.Document;
+
+import javax.print.Doc;
+import java.util.Date;
+
+public class User {
+    private String email;
+    private String password;
+    private String type;
+    private Date createdAt;
+    private Date updatedAt;
+    public User() {
+    }
+
+    public User(String email, String password, String type, Date createdAt, Date updatedAt) {
+        this.email = email;
+        this.password = password;
+        this.type = type;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Document userToDocument() {
+        return new Document()
+                .append("email", this.email)
+                .append("password", this.password)
+                .append("type", this.type)
+                .append("createdAt", this.createdAt)
+                .append("updatedAt", this.updatedAt);
+    }
+}
--- src/main/java/service/authService.java
+++ src/main/java/service/authService.java
@@ -1,16 +1,87 @@
 package service;
 
 import javax.jws.WebMethod;
-import javax.jws.WebParam;
 import javax.jws.WebService;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
+import javax.jws.WebParam;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import metier.User;
+import org.bson.Document;
+import com.mongodb.client.MongoCollection;
+import org.bson.json.JsonObject;
+import org.bson.BsonValue;
+import org.bson.types.ObjectId;
+import org.mindrot.jbcrypt.BCrypt;
+
+import java.util.regex.Pattern;
+
+import static com.mongodb.client.model.Filters.*;
 
 @WebService( name = "auth" )
 public class authService {
-    @WebMethod(operationName = "getTest")
-    public String getTest() {
-        return "Salut";
+    @WebMethod(operationName = "authentication")
+    public String Authentication(@WebParam(name = "Email") String email, @WebParam(name="Password") String password) {
+        String uri = "mongodb+srv://admin:rTm2ylmfIqj2Clq8@cluster0.wtkwefc.mongodb.net/?retryWrites=true&w=majority";
+        try (MongoClient mongoClient = MongoClients.create(uri)) {
+            MongoDatabase database = mongoClient.getDatabase("traveling");
+            MongoCollection<Document> collection = database.getCollection("user");
+
+            if(findUserByEmail(email) != null) {
+                Document currentUser = findUserByEmail(email);
+                if(!checkPassword(password, currentUser.get("password").toString())) {
+                    return "Mot de passe incorrect";
+                } else {
+                    return currentUser.toJson();
+                }
+            } else {
+                String p = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
+                if(Pattern.matches(p, email)) {
+                    String newHash = hashPassword(password);
+                    User user = new User(
+                            email,
+                            newHash,
+                            "user",
+                            new Date(),
+                            new Date()
+                    );
+                    BsonValue newUser = collection.insertOne(user.userToDocument()).getInsertedId();
+                    try {
+                        Document userFind = collection.find(eq("_id", newUser)).first();
+                        return userFind.toJson();
+                    } catch (Exception e) {
+                        return "Impossible de vous créez un compte";
+                    }
+                } else {
+                    return "L'email n'est pas du bon format";
+                }
+            }
+        } catch (Exception e) {
+            return "Une erreur est survenue";
+        }
+    }
+
+    public static String hashPassword(String password_plaintext) {
+        String salt = BCrypt.gensalt();
+        return BCrypt.hashpw(password_plaintext, salt);
+    }
+
+    public static boolean checkPassword(String password_plaintext, String stored_hash) {
+        if(null == stored_hash || !stored_hash.startsWith("$2a$"))
+            throw new java.lang.IllegalArgumentException("Invalid hash provided for comparison");
+        return BCrypt.checkpw(password_plaintext, stored_hash);
+    }
+
+    public static Document findUserByEmail (String email) {
+        String uri = "mongodb+srv://admin:rTm2ylmfIqj2Clq8@cluster0.wtkwefc.mongodb.net/?retryWrites=true&w=majority";
+        try (MongoClient mongoClient = MongoClients.create(uri)) {
+            MongoDatabase database = mongoClient.getDatabase("traveling");
+            MongoCollection<Document> collection = database.getCollection("user");
+            Document userFind = collection.find(eq("email", email)).first();
+            return userFind;
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
